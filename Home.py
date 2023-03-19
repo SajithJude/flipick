d3d5531c94c2b594a26f9f9201456af42b797252
Jude Sajith <judesajith.aj@gmail.com>
2023-03-19 03:31:14 -0400

    ssss

--- Home.py
+++ Home.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import openai
+import os
+import fitz
 
-st.write("hello world")
-# create a hello world on streamlit 
+
+st.title("Flipick XML generator GPT-4")
+
+# Upload PDF file
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+# If file was uploaded
+if uploaded_file is not None:
+    # Load PDF document
+    pdf_doc = fitz.open(stream=uploaded_file.getvalue(), filetype="pdf")
+    # Extract text from PDF pages
+    text = ""
+    for page in pdf_doc:
+        text += page.get_text()
+    # Display extracted text
+    st.write(text)
