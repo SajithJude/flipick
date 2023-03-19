3f2d449796d8a554c4d085d8121920a5bfa47799
Jude Sajith <judesajith.aj@gmail.com>
2023-03-19 05:58:47 -0400

    ssnsns

--- pages/Send_request.py
+++ pages/Send_request.py
@@ -6,4 +6,49 @@ import os
 openai.api_key = os.getenv("API_KEY")
 
 Input_content = st.session_state.content 
-st.write(Input_content)
+
+xml_str = """
+        <Course>
+                    <Topics>
+                        <Topic>
+                            <Topic_name></Topic_name>			
+                            <Contents>
+                            </Contents>
+                            <sub_Topics>
+                                <sub_Topic>
+                                    <sub_Topic_name></sub_Topic_name>
+                                    <sub_Topic_Contents>
+                                    </sub_Topic_Contents>
+                                </sub_Topic>
+                            </sub_Topics>
+                        </Topic>
+                    </Topics>
+        </Course>
+          """
+
+
+
+
+
+
+
+
+
+
+
+
+inputPrompt = " Remove the artifacts from the " + Content + " and write the content as per original without the artifacts "
+st.write(inputPrompt)
+
+response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=inputPrompt,
+        temperature=0.56,
+        max_tokens=2066,
+        top_p=1,
+        frequency_penalty=0.35,
+        presence_penalty=0
+    )
+step1Out = response.choices[0].text
+
+st.code(step1Out)
