71859553bdb94e5b9b48a5d23769921e13d768b1
Jude Sajith <judesajith.aj@gmail.com>
2023-03-19 04:58:55 -0400

    ssss

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import openai
 import os
 import fitz
 
-
+openai.api_key = os.getenv("API_KEY")
 st.title("Flipick XML generator GPT-4")
 
 
@@ -46,8 +46,24 @@ if uploaded_file is not None:
     # Load PDF document
     pdf_doc = fitz.open(stream=uploaded_file.getvalue(), filetype="pdf")
     # Extract text from PDF pages
-    text = ""
+    content = ""
     for page in pdf_doc:
-        text += page.get_text()
+        content += page.get_text()
     # Display extracted text
-    st.code(text)
+    st.code(content)
+
+    response = openai.Edit.create(
+        model="text-davinci-003",
+        input=content,
+        instruction = "Remove the artifacts from the input and write the content as per original without the artifacts",
+        temperature=0.56,
+        max_tokens=2066,
+        top_p=1,
+        frequency_penalty=0.35,
+        presence_penalty=0
+    )
+    step_1_out = response.choices[0].text
+    st.code(step_1_out)
+
+
+
