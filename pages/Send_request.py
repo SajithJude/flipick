bc54fef665131ef499caf8c30cb0a7b7b0048424
Jude Sajith <judesajith.aj@gmail.com>
2023-03-19 06:06:01 -0400

    ssss

--- pages/Send_request.py
+++ pages/Send_request.py
@@ -7,6 +7,14 @@ openai.api_key = os.getenv("API_KEY")
 
 Input_content = st.session_state.content 
 
+def trim_string(input_string):
+    tokens = input_string.split()[:4000]
+    return ' '.join(tokens)
+
+# example usage
+output_string = trim_string(Input_content)
+# print(output_string)
+
 xml_str = """
         <Course>
                     <Topics>
@@ -37,14 +45,14 @@ xml_str = """
 
 
 
-inputPrompt = " Remove the artifacts from the following pdf content, and write the content as per original without the artifacts :" + Input_content 
+inputPrompt = " Remove the artifacts from the following pdf content, and write the content as per original without the artifacts :" + output_string 
 st.write(inputPrompt)
 
 response = openai.Completion.create(
         model="text-davinci-003",
         prompt=inputPrompt,
         temperature=0.56,
-        max_tokens=2066,
+        max_tokens=4000,
         top_p=1,
         frequency_penalty=0.35,
         presence_penalty=0
