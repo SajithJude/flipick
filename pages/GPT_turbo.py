7bac55183a3059ee98b305ff8dbf9e4e7ca055a6
Jude Sajith <judesajith.aj@gmail.com>
2023-03-22 07:08:31 -0400

    sssss

---
+++ pages/GPT_turbo.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import openai
+import os
+import base64
+import json
+
+st.set_page_config(
+    page_title="Generate XML Content with GPT Turbo",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+openai.api_key = os.getenv("API_KEY")
+
+
+
+
+
+
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+if uploaded_file is not None:
+    pdf_doc = fitz.open(stream=uploaded_file.getvalue(), filetype="pdf")
+    
+    # with col2.expander("Pdf data"):
+        # Add a multi-select field to get the page numbers from the user
+    page_numbers = st.multiselect("Select page numbers", options=range(1, len(pdf_doc) + 1), default=[1])
+    
+    # Extract text from the selected page numbers
+    content = ""
+    for page_number in page_numbers:
+        page = pdf_doc[page_number - 1] # page numbers are 0-indexed in PyMuPDF
+        Input_content += page.get_text()    
+    # st.text(content)
+    st.session_state.Input_content = Input_content
+
+
+
+if st.button("Generate XML content"):
+
+    default_xml_structure = """<Course>
+            <Topics>
+                <Objectives>
+                    <Objective_name></Objective_name>
+                </Objectives>
+                <Topic>
+                    <Topic_name></Topic_name>			
+                    <Contents>
+                    </Contents>
+                    <sub_Topics>
+                        <sub_Topic>
+                            <sub_Topic_name></sub_Topic_name>
+                            <sub_Topic_Contents>
+                            </sub_Topic_Contents>
+                        </sub_Topic>
+                    </sub_Topics>
+                </Topic>
+            </Topics>
+</Course>"""
+
+    prompt_request = "Insert the contents of this pdf into the specified XML structure : " + Input_content
+
+    messages = [{"role": "system", "content":default_xml_structure }]    
+    messages.append({"role": "user", "content": prompt_request})
+
+    response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=.5,
+            max_tokens=500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+    )
+
+    outs = response["choices"][0]["message"]['content'].strip()
+    st.write(outs)
+    
+    # Create dictionary with prompt and completion
+    data = {
+        "prompt": Input_content,
+        "completion": step1Out
+    }
+    
+    # Generate download button that saves data as a JSON file
+    json_data = json.dumps(data, indent=4)
+    b64 = base64.b64encode(json_data.encode()).decode()
+    href = f'<a href="data:file/json;base64,{b64}" download="output.json">Download JSON File</a>'
+    st.markdown(href, unsafe_allow_html=True)
