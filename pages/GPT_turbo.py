d32b32f2992654c77d5fd76578f80cd6ae3168cd
Jude Sajith <judesajith.aj@gmail.com>
2023-03-25 17:27:43 -0400

    ssss

--- pages/GPT_turbo.py
+++ pages/GPT_turbo.py
@@ -7,3 +7,29 @@ if st.button("submit"):
 
     topic_names = re.findall("<Topic_name>(.*?)</Topic_name>", iput)
     st.write(topic_names)
+
+
+
+# Parse the XML string to extract the possible values for the dropdowns
+topic_names = re.findall("<Topic_name>(.*?)</Topic_name>", iput)
+sub_topic_names = re.findall("<sub_Topic_name>(.*?)</sub_Topic_name>", iput)
+
+# Create a dropdown to select the tag name
+tag_names = ["Topic_name", "sub_Topic_name"]
+selected_tag_name = st.selectbox("Select a tag name", tag_names)
+
+# Create a dropdown to select the value of the selected tag
+if selected_tag_name == "Topic_name":
+    selected_value = st.selectbox("Select a topic name", topic_names)
+else:
+    selected_value = st.selectbox("Select a sub-topic name", sub_topic_names)
+
+# Use regex to extract the tag contents based on the selected tag name and value
+regex_pattern = f"<{selected_tag_name}>{selected_value}</{selected_tag_name}>"
+tag_contents = re.findall(f"{regex_pattern}.*?</{selected_tag_name}>", iput, re.DOTALL)
+
+# Display the extracted tag contents
+if tag_contents:
+    st.write(tag_contents[0])
+else:
+    st.write("No matching tag found.")
