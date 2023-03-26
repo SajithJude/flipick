6768c5a0704f58f744e027ab3b815ddc1724a5d9
Jude Sajith <judesajith.aj@gmail.com>
2023-03-25 21:08:53 -0400

    ssss

--- pages/pymupdf.py
+++ pages/pymupdf.py
@@ -8,12 +8,11 @@ def extract_text(filename):
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
 if uploaded_file is not None:
-    pdf_doc = fitz.open(stream=uploaded_file.getvalue(), filetype="pdf")
-    for page in pdf_doc:
-        page_dims = page.MediaBox
-        content_area = page.cropBox
-        content_area.bottom = page_dims.bottom + 10  # adjust bottom margin
+    doc = fitz.open(stream=uploaded_file.getvalue(), filetype="pdf")
+    for page in doc:
+        page_dims = page.bound()
+        content_area = page.rect
+        content_area.bottom = page_dims[1] - 10  # adjust bottom margin
         content = page.get_text("text", clip=content_area)
         st.write(content)
-    pdf_doc.close()
-    
+    doc.close()
