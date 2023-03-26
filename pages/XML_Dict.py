044911cec0cf5ad1c21940df227366429b1ad527
Jude Sajith <judesajith.aj@gmail.com>
2023-03-25 21:06:30 -0400

    ssss

--- pages/XML_Dict.py
+++ pages/XML_Dict.py
@@ -8,26 +8,13 @@ def app():
     
     if st.button("Fetch content"):
 
-        # Extract all topics and their contents
-        topics = re.findall("<Topic>(.*?)</Topic>", xml_string)
-        topic_contents = re.findall("<Topic_Contents>(.*?)</Topic_Contents>", xml_string)
+        Topic = re.findall("<Topic>(.*?)</Topic>", xml_string)
+        Topic_Contents = re.findall("<Topic_Contents>(.*?)</Topic_Contents>", xml_string)
+        sub_Topic = re.findall("<sub_Topic>(.*?)</sub_Topic>", xml_string)
+        sub_Topic_Contents = re.findall("<sub_Topic_Contents>(.*?)</sub_Topic_Contents>", xml_string)
+        sub_Topic_name = re.findall("<sub_Topic_name>(.*?)</sub_Topic_name>", xml_string)
+        sub_Topic_name_Contents = re.findall("<sub_Topic_name_Contents>(.*?)</sub_Topic_name_Contents>", xml_string)
 
-        # Iterate over each topic and create an expander for it
-        for i, topic in enumerate(topics):
-            with st.expander(f"Topic {i+1}: {topic}"):
-                st.write("Topic Contents:", topic_contents[i])
-
-                # Extract all subtopics and their contents for this topic
-                sub_topics = re.findall(f"<sub_Topic>.*?<Name>{topic}</Name>.*?</sub_Topic>", xml_string, re.DOTALL)
-                sub_topic_contents = re.findall(f"<sub_Topic_Contents>.*?<Name>{topic}</Name>.*?</sub_Topic_Contents>", xml_string, re.DOTALL)
-
-                # Iterate over each subtopic and create an expander for it
-                for j, sub_topic in enumerate(sub_topics):
-                    sub_topic_name = re.findall("<sub_Topic_name>(.*?)</sub_Topic_name>", sub_topic)[0]
-                    sub_topic_name_contents = re.findall("<sub_Topic_name_Contents>(.*?)</sub_Topic_name_Contents>", sub_topic)[0]
-                    with st.expander(f"Sub Topic {j+1}: {sub_topic_name}"):
-                        st.write("Sub Topic Contents:", sub_topic_contents[j])
-                        st.write("Sub Topic Name Contents:", sub_topic_name_contents)
-
+        
 
 app()
